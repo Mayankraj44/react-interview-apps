f1e42f6a4d4a7315e9c8d879106f348ae9cbcadb
Mayankraj44 <lui.chang420@gmail.com>
2024-07-01 19:57:07 +0530

    useHover hook added to custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import GridSequence from "./grid-sequence/GridSequence";
 import DigitalClock from "./digital-clock/DigitalClock";
 import TrafficLights from "./traffic-lights/TrafficLights";
 import StartRating from "./star-rating/StartRating";
+import { useHover } from "./custom-hooks/useHover";
 
 function App() {
+  const [ref,state]=useHover()
   return (
     <>
+    <div ref={ref}>{state? 'Hoverd' : ' Not Hovered' }</div>
       {/* <TrafficLights /> */}
-      <DigitalClock />
+      {/* <DigitalClock /> */}
     </>
   );
 }
