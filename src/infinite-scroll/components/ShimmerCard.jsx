7d47172f305e0211f4dec2a7c33da631b8ff9227
Mayankraj44 <lui.chang420@gmail.com>
2024-07-20 19:53:03 +0530

    log removed

--- src/infinite-scroll/components/ShimmerCard.jsx
+++ src/infinite-scroll/components/ShimmerCard.jsx
@@ -1,5 +1,4 @@
 export default function ShimmerCard(){
-    console.log("Testing")
     return <div className="w-48 h-50">
                 <div className="w-full aspect-square bg-gray-100 shimmer-img animate rounded-md mb-2" />
                 <p  className="w-11/12 line animate"></p>
