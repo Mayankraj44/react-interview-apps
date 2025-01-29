61bb33c2128b3b4a303c5f127a26c1c6d1cd3c08
Mayankraj44 <lui.chang420@gmail.com>
2025-01-29 23:30:24 +0530

    accordion logic simplified

--- src/accordion/Accordion.jsx
+++ src/accordion/Accordion.jsx
@@ -19,25 +19,19 @@ const data = [{
     content: "Lorem ipSum"
 }]
 //Addig functionality to open at max x accordion at same time
-const expandedAccordionNum=3
+const maxOpen=3
+const defaultOpenIndex=0
 export default function Accordion() {
-    const [openIndex, setOpenIndex] = useState([0,1])
-    const handleAccordionClick = (ind) => {
-        const elementInd=openIndex.findIndex(ele=>ele===ind)
-        const newOpenIndex=[...openIndex]
-        if (elementInd!==-1) {
-            newOpenIndex.splice(elementInd,1)
-            console.log({newOpenIndex})
-            setOpenIndex(newOpenIndex)
-            return
-        }
-       if(openIndex.length==expandedAccordionNum){
-        newOpenIndex.splice(0,1)
-       }
-       newOpenIndex.push(ind)
-       setOpenIndex(newOpenIndex)
+    const [openIndex, setOpenIndex] = useState([defaultOpenIndex])
+    const handleAccordionClick = (index) => {
+        if (openIndex.includes(index)) {
+            const newOpenState = openIndex.filter((ind) => ind !== index);
+            return setIsOpen(newOpenState);
+          }
+          const newOpenState = [...openIndex];
+          newOpenState.push(index);
+          setOpenIndex(newOpenState.slice(-maxOpen));
     }
-    console.log(openIndex)
     return <div className="w-screen px-24">
         {data.map((accord, ind) => <AccordionBox key={ind} data={accord} isOpen={!!openIndex.some(ele=>ele===ind)} clickHandler={() => handleAccordionClick(ind)} />)}
     </div>
