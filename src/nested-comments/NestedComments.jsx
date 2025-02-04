0f048a99ee1bd154c0f1177d1dd260314f03e635
Mayank Raj <lui.chang420@gmail.com>
2025-02-05 01:04:14 +0530

    nested comment wip

--- src/nested-comments/NestedComments.jsx
+++ src/nested-comments/NestedComments.jsx
@@ -1,55 +1,62 @@
+import Comment from "./components/Comment";
 
-const data=[
-    {
-        author: "John Doe",
-        comment:"Testing Parent 1",
-        replies:[]
-    },
-    {
-        author: "John Doe 2",
-        comment:"Testing Parent 2",
-        replies:[
-            {
-                author: "John Doe 2.1",
-                comment:"Testing Child 2.1",
-                replies:[
-                    {
-                        author: "John Doe 2.1.1",
-                        comment:"Testing Child 2.1.1",
-                        replies:[]
-                    },
-                    {
-                        author: "John Doe 2.1.2",
-                        comment:"Testing Child 2.1.2",
-                        replies:[]
-                    },
-                ]
-            },
-            {
-                author: "John Doe 2.2",
-                comment:"Testing Parent 2.2",
-                replies:[]
-            },
-            {
-                author: "John Doe 2.3",
-                comment:"Testing Parent 2.3",
-                replies:[]
-            },
-        ]
-    },
-    {
-        author: "John Doe 3",
-        comment:"Testing Parent 3",
-        replies:[
-            {
-                author: "John Doe 3.1",
-                comment:"Testing child 3.1",
-                replies:[]
-            }
-        ]
-    }
-]
+const data = [
+  {
+    author: "John Doe",
+    comment: "Testing Parent 1",
+    replies: [],
+  },
+  {
+    author: "John Doe 2",
+    comment: "Testing Parent 2",
+    replies: [
+      {
+        author: "John Doe 2.1",
+        comment: "Testing Child 2.1",
+        replies: [
+          {
+            author: "John Doe 2.1.1",
+            comment: "Testing Child 2.1.1",
+            replies: [],
+          },
+          {
+            author: "John Doe 2.1.2",
+            comment: "Testing Child 2.1.2",
+            replies: [],
+          },
+        ],
+      },
+      {
+        author: "John Doe 2.2",
+        comment: "Testing Parent 2.2",
+        replies: [],
+      },
+      {
+        author: "John Doe 2.3",
+        comment: "Testing Parent 2.3",
+        replies: [],
+      },
+    ],
+  },
+  {
+    author: "John Doe 3",
+    comment: "Testing Parent 3",
+    replies: [
+      {
+        author: "John Doe 3.1",
+        comment: "Testing child 3.1",
+        replies: [],
+      },
+    ],
+  },
+];
 
-export default function NestedComments(){
-    return <>NEsted Comments</>
+export default function NestedComments() {
+  return (
+    <>
+      {data?.map((comments, index) => (
+        <Comment data={comments} key={index} />
+      ))}
+    </>
+  );
 }
--- src/nested-comments/components/Comment.jsx
+++ src/nested-comments/components/Comment.jsx
@@ -1,5 +1,26 @@
-export default function Comment(){
-    return <>
-        <div></div>
-    </>
+export default function Comment({ data }) {
+  return (
+    <div className="ml-8 ">
+      <div className="mb-4 ">
+        <div className="flex gap-1 items-center">
+          <img
+            className="h-8 rounded-full"
+            src="https://picsum.photos/200"
+            alt="user icon"
+          />
+          <p className="font-bold mb-2 ">{data?.author}</p>
+        </div>
+        <div className="relative ">
+          <span className="absolute h-full w-[1px] bg-red-500 left-4 top-1" />
+          <p className="ml-9 ">{data?.comment}</p>
+          <div className="mt-4 ">
+            {!!data?.replies.length &&
+              data?.replies?.map((reply, index) => (
+                <Comment data={reply} key={index} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
